88d02f0c4a3166403300ad60ca38d9619363a2df
cpfl300 <cpfl300@naver.com>
2013-08-23 22:02:34 +0900

    초기화 상태에서 공을 굴릴 때 몇 번째 투구할 차례인지를 물으면 1Frame, 1차 시기라는 것을 출력하게 한다.

---
+++ BowlingGame/src/BowlingGame.java
@@ -0,0 +1,10 @@
+
+public class BowlingGame {
+	
+	int currnetFrame = 1;
+	int currentTry = 1;
+	
+	public void getCurrentTry() {
+		System.out.printf("%d Frame, %d차 시기 입니다.\n", currnetFrame, currentTry);
+	}
+}
